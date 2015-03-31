6adc1e93ba2b0c24fc7ed7de704df1da77b04659
Joan Leon <joan.leon@gmail.com>
2015-03-31 12:22:26 +0200

    Added change in InfoJobs template for filter (with helper) disabled sections.

--- styleguide/templates/helpers/disable.js
+++ styleguide/templates/helpers/disable.js
@@ -6,9 +6,11 @@ module.exports.register = function(handlebars) {
     * @usage: {{isDisabled}}
     */
     handlebars.registerHelper('isDisabled', function(string, options) {
-        if(string.indexOf('#disabled#') > -1) {
+
+        if( string.indexOf('#disabled#') > -1 ) {
             return options.fn(this);
         }
+
         return options.inverse(this);
     });
 
