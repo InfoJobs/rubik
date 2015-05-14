08fa20ae2acf7c4af0ea1325e9d9003ea208a755
Joan Leon <joan.leon@gmail.com>
2015-05-14 12:31:30 +0200

    Modifiers & functions loadCSS, loadJS

---
+++ js/loadCSS.js
@@ -0,0 +1,21 @@
+/*!
+loadJS: load a JS file asynchronously.
+[c]2014 @scottjehl, Filament Group, Inc.
+(Based on http://goo.gl/REQGQ by Paul Irish).
+Licensed MIT
+https://github.com/filamentgroup/loadJS
+*/
+function loadJS( src, callback ){
+    "use strict";
+    var ref = w.document.getElementsByTagName( "script" )[ 0 ];
+    var script = w.document.createElement( "script" );
+    script.src = src;
+    script.async = true;
+    ref.parentNode.insertBefore( script, ref );
+
+    if (callback && typeof(callback) === "function") {
+        script.onload = callback;
+    }
+
+    return script;
+}
