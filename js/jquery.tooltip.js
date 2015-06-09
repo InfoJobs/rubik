b487050c3aa89661de33dc6df0348cb3b9ae9364
Pablo Gonzalez <pablo.regresion@gmail.com>
2015-06-09 09:52:24 +0200

    tooltip close on esc key

--- js/jquery.tooltip.js
+++ js/jquery.tooltip.js
@@ -11,6 +11,14 @@
 			// Defaul options
 			var settings = $.extend({}, $.fn.toolTip.defaults, options );
 
+	        // Close on ESC key
+	        $(document).on('keyup', function(event) {
+	            if(event.keyCode === 27) {
+	                $.fn.toolTip.closeTooltip();
+	            }
+	        });
+
+
 			return this.each(function() {
 
 				var $this 		= $( this ),
@@ -41,6 +49,8 @@
 				});
 
 				$this.find('.tooltip').prepend(closeButton);
+
+
 
 				$this.find('.close-tooltip').click(function(event) {
 					event.preventDefault();
