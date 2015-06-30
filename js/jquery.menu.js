ee98afc7f9be0b3c88786ff21c57e68709035700
Pablo Gonzalez <pablo.regresion@gmail.com>
2015-06-30 17:28:26 +0200

    javascript menu OK para review (sin resize)

--- js/jquery.menu.js
+++ js/jquery.menu.js
@@ -1,16 +1,17 @@
 $(function() {
+
+	function closeMenu() {
+		$('.nav-mobile').addClass('fade-out');
+		$('.wrapper').removeClass('wrap-overlay');
+	    setTimeout(function(){
+	        $('.nav-mobile').removeClass('open-menu fade-out fade-in');
+	     },1000);
+	}
+
     $('.js-open-menu').click(function(){
 
         $(this).next('.nav-mobile').addClass('fade-in open-menu');
 		$('.wrapper').addClass('wrap-overlay');
-
-		function closeMenu() {
-			$('.nav-mobile').addClass('fade-out');
-			$('.wrapper').removeClass('wrap-overlay');
-		    setTimeout(function(){
-		        $('.nav-mobile').removeClass('open-menu fade-out fade-in');
-		     },1000);
-		}
 
 		if ($('.nav-mobile').hasClass('open-menu')) {
 
@@ -22,19 +23,16 @@ $(function() {
 			     event.stopPropagation();
 			 });
 
-		    // Close on ESC key
-		    $(document).on('keyup', function(event) {
-
-		        if(event.keyCode === 27) {
-					closeMenu();
-		        }
-
-		    });
-
 		}
-
 
     });
 
+    // Close on ESC key
+    $(document).on('keyup', function(event) {
+        if(event.keyCode === 27 && $('.nav-mobile').hasClass('open-menu')) {
+			closeMenu();
+        }
+
+    });
 
 });
