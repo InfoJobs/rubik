8cedc46c09e7fbad58940a666071fa8e2a008122
Norbert Guasch <norbertguasch@gmail.com>
2015-07-27 11:21:42 +0200

    fix js

--- js/changeButtonText.js
+++ js/changeButtonText.js
@@ -1,20 +1,23 @@
   function changeButtonText(){
-    var elButton = document.querySelector('.js-btn-change');
-    var elText = document.querySelector('.js-btn-change span');
+    var elButton = document.querySelectorAll('.js-btn-change');
+    var elText = document.querySelectorAll('.js-btn-change span');
     var mq = window.matchMedia( "(min-width: 769px)" );
-    if (mq.matches) {
-      elButton.classList.remove('iconfont-Close');
-      elText.classList.remove('hide');
 
-      elButton.style.marginRight = "0";
-      elButton.style.paddingLeft = "10px";
-    }
-    else{
-      elButton.classList.add('iconfont-Close');
-      elText.classList.add('hide');
+    for(var i=0; i < elButton.length; i++){
+      if (mq.matches) {
+        elButton[i].classList.remove('iconfont-Close');
+        elText[i].classList.remove('hide');
 
-      elButton.style.marginRight = "-10px";
-      elButton.style.paddingLeft = "12px";
+        elButton[i].style.marginRight = "0";
+        elButton[i].style.paddingLeft = "10px";
+      }
+      else{
+        elButton[i].classList.add('iconfont-Close');
+        elText[i].classList.add('hide');
+
+        elButton[i].style.marginRight = "-10px";
+        elButton[i].style.paddingLeft = "12px";
+      }
     }
   }
 
