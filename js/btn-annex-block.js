134dccb2dc1b656d46728b2710b8e2dd79611892
Laura Arnedo <laura.arnedo@gmail.com>
2015-10-06 15:48:47 +0200

    Improve btn-annex-block.js

--- js/btn-annex-block.js
+++ js/btn-annex-block.js
@@ -7,7 +7,7 @@ function btnHeight() {
     [].forEach.call( elBtnGroup, function( target ){
         elBtn = target.querySelector('.btn');
 
-        if (elBtn.parentNode.offsetHeight > elBtn.offsetHeight) {
+        if (target.offsetHeight > elBtn.offsetHeight) {
             elBtn.classList.add('margin-bottom');
         }
 
