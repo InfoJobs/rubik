dba2b09b3c9de72720315530bdee8c2c399c7acb
Pablo Gonzalez <pablo.regresion@gmail.com>
2016-04-27 12:35:57 +0200

    move list items

---
+++ js/move-list-items.js
@@ -0,0 +1,28 @@
+(function(){
+
+	function moveChecKed() {
+
+        var elMoveList = document.querySelector('.js-to-move');
+		var elMoveListItems = document.querySelectorAll('.js-to-move label:first-of-type');
+		var elMainListItems = document.querySelectorAll('.js-main-list > li');
+        var elMoved;
+
+
+		[].forEach.call( elMoveListItems, function( target ){
+            target.onclick = function(){
+
+				elMoved = target.parentNode;
+				if(elMoved.parentNode == elMoveList) {
+					elMoved.parentNode.parentNode.querySelector('.js-main-list').insertBefore(elMoved, elMainListItems[elMainListItems.length-1]);
+					elMoveListItems = document.querySelectorAll('.js-to-move label');
+				}
+
+            };
+
+		});
+
+	}
+
+	window.addEventListener('load', moveChecKed, false);
+
+}());
