ab06392c7c37f35eac6d8b3de9352c04865b7829
Norbert Guasch <norbertguasch@gmail.com>
2016-05-02 17:09:29 +0200

    refactor input-appearance-sticker.js

--- js/input-appearance-sticker.js
+++ js/input-appearance-sticker.js
@@ -1,48 +1,49 @@
-function init() {
-	[].forEach.call( document.querySelectorAll('.js-appearance-sticker ul'), function( target ){
-        target.classList.add('hide');
-    });
-
-    [].forEach.call( document.querySelectorAll('.js-container-sticker'), function( target ) {
-		[].forEach.call( target.querySelectorAll(':scope > li > label'), function( label ){
-			var span = document.createElement('span');
-			span.classList.add('level');
-			label.appendChild(span);
-
-			label.onclick=function(event){
-				label.parentNode.querySelector('ul').classList.toggle('hide');
-				if(label.parentNode.querySelector('input').checked) {
-					event.preventDefault();
-				}
-			};
-
-			label.querySelector('.sticker-close').onclick=function(event) {
+(function(){
+	function init() {
+		// show/hide level list
+		function showLevels(label,event){
+			label.parentNode.querySelector('ul').classList.toggle('hide');
+			if(label.parentNode.querySelector('input').checked) {
 				event.preventDefault();
-				event.stopPropagation();
-				label.parentNode.querySelector('input').checked = false;
-				label.parentNode.querySelector('ul').classList.add('hide');
-				label.querySelector('.level').innerHTML = '';
-			};
+			}
+		}
+
+		//ocultar las listas que contienen los niveles
+		[].forEach.call( document.querySelectorAll('.js-appearance-sticker ul'), function( target ){
+	        target.classList.add('hide');
+	    });
+
+
+	    [].forEach.call( document.querySelectorAll('.js-container-sticker'), function( target ) {
+			[].forEach.call( target.querySelectorAll(':scope > li > label'), function( label ){
+				var span = document.createElement('span');
+				span.classList.add('level');
+				label.appendChild(span);
+
+				label.onclick = function(event) {showLevels(this,event)};
+
+				label.querySelector('.sticker-close').onclick=function(event) {
+					event.preventDefault();
+					event.stopPropagation();
+					label.parentNode.querySelector('input').checked = false;
+					label.parentNode.querySelector('ul').classList.add('hide');
+					label.querySelector('.level').innerHTML = '';
+				};
+			});
+
+		    [].forEach.call( target.querySelectorAll(':scope > li > ul > li > label'), function( level ){
+		        level.onclick=function(){
+		            level.parentNode.parentNode.parentNode.querySelector('.level').innerHTML = level.textContent;
+					level.parentNode.parentNode.classList.add('hide');
+		        };
+		    });
 		});
 
-	    [].forEach.call( target.querySelectorAll(':scope > li > ul > li > label'), function( level ){
-	        level.onclick=function(){
-	            level.parentNode.parentNode.parentNode.querySelector('.level').innerHTML = level.textContent;
-				level.parentNode.parentNode.classList.add('hide');
-	        };
-	    });
-	});
-}
-window.addEventListener('load', init);
-
-(function(){
-
-	function moveChecKed() {
 		var elMoveListItems = document.querySelectorAll('.js-move-list-items');
         var elMoved;
 
 		[].forEach.call( elMoveListItems, function( target ){
-
+			//ocultar la lista de 'otros' idiomas
 			var elListItemsToMoved = target.querySelector('.js-list-to-move');
 			elListItemsToMoved.classList.toggle('hide');
 
@@ -64,10 +65,7 @@ window.addEventListener('load', init);
 					if(elMoved.parentNode == elListItemsToMoved) {
 						target.querySelector('.js-main-list').insertBefore(elMoved, elMainListItems[elMainListItems.length-1]);
 					}
-					label.parentNode.querySelector('ul').classList.toggle('hide');
-					if(label.parentNode.querySelector('input').checked) {
-						event.preventDefault();
-					}
+					showLevels(this,event);
 	            };
 
 			});
@@ -76,6 +74,6 @@ window.addEventListener('load', init);
 
 	}
 
-	window.addEventListener('load', moveChecKed, false);
+	window.addEventListener('load', init, false);
 
 }());
