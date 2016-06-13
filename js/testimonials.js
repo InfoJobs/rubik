2dc674e65e168f4c0937703eff1d629b8246e915
Norbert Guasch <norbertguasch@gmail.com>
2016-06-13 17:31:55 +0200

    refactor testimonials

--- js/testimonials.js
+++ js/testimonials.js
@@ -3,6 +3,7 @@ function insertAfter(newNode, referenceNode) {
 };
 
 var nCurrentTestimonialsBody = 0;
+var aHeight = [];
 
 function testimonials(){
 
@@ -12,14 +13,18 @@ function testimonials(){
   var aTestimonialsBody = document.querySelectorAll('.testimonials-body');
   var nTestimonialsBody = 0;
 
-
   [].forEach.call(aTestimonialsBody, function(content) {
+    content.style.padding = '0 5%';
+    aHeight.push(content.clientHeight);
+
     if(content.clientHeight >= testimonialsBodyHeight) {
       testimonialsBodyHeight = content.clientHeight;
     }
-    content.classList.add('hide');
+
     nTestimonialsBody = nTestimonialsBody + 1;
   });
+
+  console.log(aHeight);
 
   var ul = document.createElement("ul");
   var div = document.createElement("div");
@@ -37,27 +42,13 @@ function testimonials(){
   }
 
   var testimonialsFooter = document.querySelector('.testimonials-footer');
-  var lastTestimonialBody = aTestimonialsBody[nTestimonialsBody - 1];
-  insertAfter(testimonialsFooter, lastTestimonialBody);
-  //
-  // if(document.querySelector('.testimonials').clientWidth < 768){
-  //   var elToMove = document.querySelector('.pull-right');
-  //   elToMove.classList.add('text-center');
-  //   elToMove.querySelector('span').style.display = 'block';
-  //   elToMove.querySelector('.btn').classList.add('margin-top');
-  //
-  //   insertAfter(elToMove, testimonialsFooter);
-  // }
+  var containerTestimonialBody = document.querySelector('.testimonials-body-container');
+  insertAfter(testimonialsFooter, containerTestimonialBody);
 
   var testimonialsFooterHeight = document.querySelector('.testimonials-footer').clientHeight + 20;
-
   var testimonialsTotalHeight = testimonialsBodyHeight + testimonialsHeaderHeight + testimonialsFooterHeight + 40;
 
   document.querySelector('.testimonials').style.height = testimonialsTotalHeight + 'px';
-
-
-
-  aTestimonialsBody[0].classList.remove('hide');
 
   var aPagination = document.querySelectorAll('.js-testimonials-pagination li');
 
@@ -65,7 +56,7 @@ function testimonials(){
     clearInterval(interval);
   });
   document.querySelector('.testimonials').addEventListener('mouseout',function(){
-    interval = setInterval(automaticTestimonials,6000);
+    interval = setInterval(automaticTestimonials,2000);
   });
 
   for(var i = 0; i < aPagination.length; i++){
@@ -74,22 +65,21 @@ function testimonials(){
       document.querySelector('.testimonials').addEventListener('mouseout',function(){
         clearInterval(interval);
       });
-      showTestimonialsBody(this,false);
+      showTestimonialsBody(this);
     }.bind(i));
   }
 
-  showTestimonialsBody(0,true);
+  showTestimonialsBody(0);
 
-  var countAutomaticTestimonials = 1;
+  var countAutomaticTestimonials = 0;
   function automaticTestimonials() {
-    showTestimonialsBody(countAutomaticTestimonials,true);
+    showTestimonialsBody(countAutomaticTestimonials);
     countAutomaticTestimonials = (countAutomaticTestimonials + 1) % aTestimonialsBody.length;
   };
 
-  var interval = setInterval(automaticTestimonials,6000);
+  var interval = setInterval(automaticTestimonials,2000);
 
-
-  if(document.querySelector('.testimonials').clientWidth < 768){
+  if(document.querySelector('.testimonials').clientWidth < 769){
     document.querySelector('.testimonials').style.height = 100 + '%';
     clearInterval(interval);
     var elToMove = document.querySelector('.pull-right');
@@ -101,92 +91,81 @@ function testimonials(){
   }
 }
 
-function showTestimonialsBody(item,automatic){
+function showTestimonialsBody(item){
   var aTestimonialsBody = document.querySelectorAll('.testimonials-body');
   var aPagination = document.querySelectorAll('.js-testimonials-pagination li');
-  [].forEach.call(aTestimonialsBody, function(content) {
-    content.classList.add('hide');
-  });
+  var posTestimonials = document.querySelector('.testimonials-body').clientWidth * -1;
 
-  aTestimonialsBody[item].classList.remove('fade-out-left','fade-out-right','hide');
-
-  aTestimonialsBody[item].classList.add(nCurrentTestimonialsBody > item && !automatic ? 'fade-in-right' : 'fade-in-left');
-
+  document.querySelector('.testimonials-body-container').style.left = (posTestimonials * item) + 'px';
   aPagination[nCurrentTestimonialsBody].classList.remove('active');
   aPagination[item].classList.add('active');
-
   nCurrentTestimonialsBody = item;
+  if(document.querySelector('.testimonials').clientWidth < 769){
+    document.querySelector('.testimonials-body-container').style.height = aHeight[item] + 'px';
+    aTestimonialsBody[item].style.padding = '0 5%';
+  }
 }
 
 window.addEventListener('load', testimonials);
-window.addEventListener('resize', testimonials);
 
+// var xDown = null;
+// var yDown = null;
+//
+//   var iTouch = 0;
+//
+// function handleTouchStart(evt) {
+//     xDown = evt.touches[0].clientX;
+//     yDown = evt.touches[0].clientY;
+// };
+//
+// function handleTouchMove(evt) {
+//     if ( ! xDown || ! yDown ) {
+//         return;
+//     }
+//
+//     var xUp = evt.touches[0].clientX;
+//     var yUp = evt.touches[0].clientY;
+//
+//     var xDiff = xDown - xUp;
+//     var yDiff = yDown - yUp;
+//
+//     if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+//         if ( xDiff > 0 ) {
+//             var aTestimonialsBody = document.querySelectorAll('.testimonials-body');
+//
+//             if (iTouch < aTestimonialsBody.length-1) {
+//               aTestimonialsBody[iTouch].classList.add('fade-out','hide');
+//               aTestimonialsBody[iTouch].classList.remove('fade-in-left');
+//
+//               aTestimonialsBody[iTouch+1].classList.remove('fade-out','hide');
+//               aTestimonialsBody[iTouch+1].classList.add('fade-in-left');
+//
+//               iTouch = iTouch+1;
+//             }
+//         } else {
+//             var aTestimonialsBody = document.querySelectorAll('.testimonials-body');
+//
+//             if (iTouch < aTestimonialsBody.length && iTouch > 0) {
+//
+//               aTestimonialsBody[iTouch].classList.add('fade-out','hide');
+//               aTestimonialsBody[iTouch].classList.remove('fade-in-right');
+//
+//               aTestimonialsBody[iTouch-1].classList.remove('fade-out','hide');
+//               aTestimonialsBody[iTouch-1].classList.add('fade-in-right');
+//
+//               iTouch = iTouch-1;
+//             }
+//         }
+//         var aPagination = document.querySelectorAll('.js-testimonials-pagination li');
+//         aPagination[nCurrentTestimonialsBody].classList.remove('active');
+//         aPagination[iTouch].classList.add('active');
+//
+//         nCurrentTestimonialsBody = iTouch;
+//     }
+//     /* reset values */
+//     xDown = null;
+//     yDown = null;
+// };
 
-
-
-
-
-
-
-
-var xDown = null;
-var yDown = null;
-
-  var iTouch = 0;
-
-function handleTouchStart(evt) {
-    xDown = evt.touches[0].clientX;
-    yDown = evt.touches[0].clientY;
-};
-
-function handleTouchMove(evt) {
-    if ( ! xDown || ! yDown ) {
-        return;
-    }
-
-    var xUp = evt.touches[0].clientX;
-    var yUp = evt.touches[0].clientY;
-
-    var xDiff = xDown - xUp;
-    var yDiff = yDown - yUp;
-
-    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-        if ( xDiff > 0 ) {
-            var aTestimonialsBody = document.querySelectorAll('.testimonials-body');
-
-            if (iTouch < aTestimonialsBody.length-1) {
-              aTestimonialsBody[iTouch].classList.add('fade-out','hide');
-              aTestimonialsBody[iTouch].classList.remove('fade-in-left');
-
-              aTestimonialsBody[iTouch+1].classList.remove('fade-out','hide');
-              aTestimonialsBody[iTouch+1].classList.add('fade-in-left');
-
-              iTouch = iTouch+1;
-            }
-        } else {
-            var aTestimonialsBody = document.querySelectorAll('.testimonials-body');
-
-            if (iTouch < aTestimonialsBody.length && iTouch > 0) {
-
-              aTestimonialsBody[iTouch].classList.add('fade-out','hide');
-              aTestimonialsBody[iTouch].classList.remove('fade-in-right');
-
-              aTestimonialsBody[iTouch-1].classList.remove('fade-out','hide');
-              aTestimonialsBody[iTouch-1].classList.add('fade-in-right');
-
-              iTouch = iTouch-1;
-            }
-        }
-        var aPagination = document.querySelectorAll('.js-testimonials-pagination li');
-        aPagination[nCurrentTestimonialsBody].classList.remove('active');
-        aPagination[iTouch].classList.add('active');
-
-        nCurrentTestimonialsBody = iTouch;
-    }
-    /* reset values */
-    xDown = null;
-    yDown = null;
-};
-
-document.addEventListener('touchstart', handleTouchStart, false);
-document.addEventListener('touchmove', handleTouchMove, false);
+// document.addEventListener('touchstart', handleTouchStart, false);
+// document.addEventListener('touchmove', handleTouchMove, false);
