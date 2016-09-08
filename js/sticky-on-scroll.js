d7c6c4a3663a995a2374e1dfd5e0c40e063936b3
Pablo Gonzalez <pablo.regresion@gmail.com>
2016-09-08 17:23:52 +0200

    sticky scroll resize

--- js/sticky-on-scroll.js
+++ js/sticky-on-scroll.js
@@ -2,26 +2,37 @@
 (function(){
   var stickyElement;
   var stickyElementOffset;
+  var stickyElementDesktop;
 
   window.onload = function() {
     stickyElement = $('.js-sticky');
-
+    stickyElementDesktop = $('.js-sticky-desktop');
   }
 
   function offsetElement() {
-    if(stickyElement) {
+
+    if(stickyElement.length) {
+      stickyElement.removeClass('sticky');
       stickyElementOffset = stickyElement.offset().top;
       return stickyElementOffset;
     }
 
+    if(stickyElementDesktop.length) {
+      stickyElement.removeClass('sticky');
+      stickyElementOffset = stickyElementDesktop.offset().top;
+      return stickyElementOffset;
+    }
+
   }
+
   function sticky() {
-    if(stickyElement) {
+    if(stickyElement.length) {
 
       var widthParent = stickyElement.parent().width();
       stickyElement.css('max-width', widthParent);
 
       if ($(window).scrollTop() >= stickyElementOffset) {
+
           stickyElement.addClass('sticky');
       }
       else {
@@ -31,7 +42,10 @@
 
   }
 
+
   window.addEventListener('load', offsetElement);
+  window.addEventListener('resize', offsetElement);
+
   window.addEventListener('scroll', sticky);
   window.addEventListener('load', sticky);
   window.addEventListener('resize', sticky);
