c99629ad1c69c0105e29b43b1778b132c3e93a76
Pablo Gonzalez <pablo.regresion@gmail.com>
2016-09-20 15:57:57 +0200

    scroll and  margin container

--- js/sticky-on-scroll.js
+++ js/sticky-on-scroll.js
@@ -3,7 +3,9 @@
   var stickyElement = $('.js-sticky');
   var stickyElementDesktop = $('.js-sticky-desktop');
   var stickyElementOffset;
-
+  var totalViewport;
+  var scrollY;
+  var heightSide;
 
   window.onload = function() {
     stickyElement = $('.js-sticky');
@@ -18,6 +20,8 @@
     function returnOffset(e) {
       if(e.offset() != null) {
         e.removeClass('sticky');
+        e.css('margin-top', 'inherit');
+
         stickyElementOffset = e.offset().top;
         return stickyElementOffset;
       }
@@ -40,23 +44,51 @@
     }
 
     function makeMeSticky(el) {
+
+      scrollY = $(window).scrollTop();
+      if(el.parents().hasClass('container-expanded')) {
+        scrollY += parseInt(styleContainer().marginTop, 10);
+
+      }
+
       var widthParent = el.parent().width();
       el.css('max-width', widthParent);
 
-      if ($(window).scrollTop() > stickyElementOffset) {
-          el.addClass('sticky');
+      if (scrollY > stickyElementOffset) {
+        el.addClass('sticky');
+
+        if(el.parents().hasClass('container-expanded')) {
+          el.css('margin-top', styleContainer().marginTop);
+        }
+
       } else {
         el.removeClass('sticky');
-      }
 
-      if(el.hasClass('sticky') && el.parents().hasClass('container-expanded')) {
-        el.css('margin-top', '10px');
-      } else {
-        el.css('margin-top', 'inherit');
+        if(el.parents().hasClass('container-expanded')) {
+          el.css('margin-top', 'inherit');
+        }
+
       }
 
     }
 
+  }
+
+  function scrollSideBar() {
+    stickyElementDesktop.css('height', 'auto');
+    heightSide = stickyElementDesktop.height();
+    totalViewport = window.innerHeight;
+
+    if (heightSide > totalViewport) {
+      stickyElementDesktop.css('height', totalViewport);
+      stickyElementDesktop.css('overflow-y', 'auto');
+    }
+
+  }
+
+  function styleContainer() {
+    var style = document.querySelector('.container-expanded').currentStyle || window.getComputedStyle(document.querySelector('.container-expanded'));
+    return style
   }
 
   window.addEventListener('load', offsetElement);
@@ -66,4 +98,9 @@
   window.addEventListener('load', sticky);
   window.addEventListener('resize', sticky);
 
+  window.addEventListener('load', scrollSideBar);
+  window.addEventListener('resize', scrollSideBar);
+
 })();
+
+//https://github.com/InfoJobs/rubik/pull/68/files
