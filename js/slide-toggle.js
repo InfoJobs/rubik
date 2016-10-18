678633aaf990740748598115c0b21ddad4c7cf64
Pablo Gonzalez <pablo.regresion@gmail.com>
2016-10-18 14:52:52 +0200

    updated from empleo

--- js/slide-toggle.js
+++ js/slide-toggle.js
@@ -1,26 +1,38 @@
 (function(){
 
-		var eClickItem = document.querySelectorAll('.js-slide-toggle');
-		var eDivToggle = document.querySelector('.toggle');
-
 	function slideToggle() {
+
+		var eClickItem = document.querySelectorAll('.js-collapsible-accordion-toggle');
 
 		[].forEach.call( eClickItem, function( target ){
 
-			        target.onclick = function(event){
-			            eDivToggle.classList.toggle('hide');
-                  event.stopPropagation();
-			        };
-
+	        target.onclick = function(e){
+	        	e.preventDefault();
+	        	target.parentNode.querySelector('.toggle').classList.toggle('hide');
+	        	target.querySelector('.arrow').classList.toggle('script');
+	        	target.querySelector('.arrow').classList.toggle('iconfont-ArrowDown');
+	        	target.querySelector('.arrow').classList.toggle('iconfont-ArrowUp');
+	        };
 		});
-
-    document.querySelector('html').onclick = function() {
-      eDivToggle.classList.add('hide');
-
-    }
-
 	}
 
+
+	function turnArrow() {
+
+		var eContentItem = document.querySelectorAll('.collapsible-accordion-content');
+
+		[].forEach.call( eContentItem, function( target ){
+
+			if (!target.classList.contains('hide')) {
+				target.parentNode.querySelector('.arrow').classList.toggle('script');
+				target.parentNode.querySelector('.arrow').classList.toggle('iconfont-ArrowUp');
+				target.parentNode.querySelector('.arrow').classList.toggle('iconfont-ArrowDown');
+	        }
+		});
+	};
+
+	window.addEventListener('load', turnArrow);
 	window.addEventListener('load', slideToggle);
+
 
 }());
