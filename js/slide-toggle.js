48cc66f9f2b62d9b751d5d3b0788a8261cce389a
PablitoIJ <PablitoGS@users.noreply.github.com>
2018-10-01 11:08:27 +0200

    Feature/estadisticas empresas (#242)
    * update forms
    
    * tables and forms updated
    
    * updated tables
    
    * fix misspeling
    
    * link a visibilidad salario
    
    * link a visibilidad salario small
    
    * fix slide toggle

--- js/slide-toggle.js
+++ js/slide-toggle.js
@@ -3,16 +3,34 @@
 	function slideToggle() {
 
 		var eClickItem = document.querySelectorAll('.js-collapsible-accordion-toggle');
+		var contentToggle;
+		var arrowToggle;
 
 		[].forEach.call( eClickItem, function( target ){
 
+			var contentToggle = target.parentNode.querySelector('.toggle');
+
+			function setTextToggle() {
+	    		var attribute = contentToggle.classList.contains('hide') ? "data-text-show" : "data-text-hide"
+		    	target.textContent = target.getAttribute(attribute) || target.textContent ;
+			}
+
 	        target.onclick = function(e){
 	        	e.preventDefault();
-	        	target.parentNode.querySelector('.toggle').classList.toggle('hide');
-	        	target.querySelector('.arrow').classList.toggle('script');
-	        	target.querySelector('.arrow').classList.toggle('iconfont-ArrowDown');
-	        	target.querySelector('.arrow').classList.toggle('iconfont-ArrowUp');
+	        	contentToggle.classList.toggle('hide');
+	        	arrowToggle = target.querySelector('.arrow');
+	        	if(arrowToggle) {
+	        		arrowToggle.classList.toggle('script');
+	        		arrowToggle.classList.toggle('iconfont-ArrowDown');
+	        		arrowToggle.classList.toggle('iconfont-ArrowUp');
+	        	}
+	        	setTextToggle();
 	        };
+
+	        target.onload = function(){
+	        	setTextToggle()
+	        }();
+
 		});
 	}
 
