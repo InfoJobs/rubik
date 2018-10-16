918181adfdaceae0f62e3911d1e2afa6481c4012
Pablo Gonzalez <pablo.regresion@gmail.com>
2018-10-16 10:50:04 +0200

    sidebar sticky only desktop

--- js/sticky.js
+++ js/sticky.js
@@ -1,51 +1,89 @@
 
 (function(){
+  var wrapper;
   var stickyContent;
+  var stickySidebar;
   var posStickyContent;
+  var posStickySidebar;
   var scrollY;
+  var desktop;
 
   function stickyOnscroll() {
-    stickyContent = document.querySelector('.js-sticky');
+    desktop = window.innerWidth > 768;
+    wrapper = document.querySelector('.wrapper');
+    stickyContent = document.querySelector('.js-sticky-content');
+    stickySidebar = document.querySelector('.js-sticky-sidebar');
     scrollY = window.pageYOffset;
+
     if(stickyContent) {
-      posStickyContent = stickyContent.getBoundingClientRect().top + scrollY;
-      setWidth();
-      toggleSticky();
-
-      window.addEventListener('scroll', function() {
-        toggleSticky();
-      });
-
-      window.addEventListener('resize', function() {
-        setWidth();
-      });
+      posStickyContent = stickyContent.getBoundingClientRect().top + scrollY ;
+      setWidth(stickyContent);
     }
+
+    if(stickySidebar && desktop) {
+      posStickySidebar = stickySidebar.getBoundingClientRect().top + scrollY;
+      setWidth(stickySidebar);
+    }
+
+    toggleSticky();
+
+    window.addEventListener('scroll', function() {
+      toggleSticky();
+    });
+
   }
 
   function toggleSticky() {
     scrollY = window.pageYOffset;
-    posStickyContent < scrollY ? stickyContent.classList.add("sticky") : stickyContent.classList.remove("sticky");
 
-    if(stickyContent.classList.contains("sticky")) {
-      document.querySelector('.wrapper').style.marginTop =  stickyContent.offsetHeight + parseInt(parentMarginContainer().marginTop, 10) + 'px';
-    } else {
-      document.querySelector('.wrapper').removeAttribute("style");
+    if(stickyContent) {
+      toggleClass(posStickyContent , stickyContent);
+
+      if(stickyContent.classList.contains("sticky")) {
+        wrapper.style.marginTop = stickyContent.offsetHeight + parseInt(parentMarginContainer().marginTop, 10) + 'px';
+      } else {
+        wrapper.removeAttribute("style");
+      }
     }
+    if(stickySidebar && desktop) {
+      toggleClass(posStickySidebar , stickySidebar);
+    }
+
+    if(stickyContent.classList.contains("sticky") && stickySidebar) {
+      posStickySidebar = posStickySidebar - stickyContent.offsetHeight;
+      stickySidebar.style.top = stickyContent.offsetHeight + parseInt(parentMarginContainer().marginTop, 10) + 'px';
+    } else {
+        stickySidebar.style.top = 0;
+        stickySidebar.classList.remove("sticky");
+    }
+
   }
 
-  function setWidth() {
-    var widthSticky = stickyContent.parentNode.clientWidth;
-    stickyContent.style.maxWidth = widthSticky + 'px';
+  function toggleClass(posSticky , stickyElement) {
+    posSticky < scrollY ? stickyElement.classList.add("sticky") : stickyElement.classList.remove("sticky");
+  }
+
+  function setWidth(stickyElement) {
+    var widthSticky = stickyElement.parentNode.clientWidth;
+    stickyElement.style.maxWidth = widthSticky + 'px';
   }
 
   function parentMarginContainer() {
-    var parentContainer = stickyContent.parentNode;
+    var parentContainer =stickyContent.parentNode;
     var styleMarginTop = parentContainer.currentStyle || window.getComputedStyle(parentContainer);
     return styleMarginTop;
   }
 
   window.addEventListener('load', function() {
       stickyOnscroll();
+  });
+
+  window.addEventListener('resize', function() {
+    stickyOnscroll();
+    setWidth(stickySidebar);
+    setWidth(stickyContent);
+
+
   });
 
 })();
