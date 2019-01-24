6ec914d5f29d45ada025382fa065d6d55ee101bd
Norbert Guasch <norbertguasch@gmail.com>
2019-01-24 12:35:13 +0100

    new tooltip position

--- js/jquery.tooltip.js
+++ js/jquery.tooltip.js
@@ -7,92 +7,93 @@
 ;(function ( $, window, document, undefined ) {
 
 
-		$.fn.toolTip = function( options ) {
-			// Defaul options
-			var settings = $.extend({}, $.fn.toolTip.defaults, options );
+	$.fn.toolTip = function( options ) {
+		// Defaul options
+		var settings = $.extend({}, $.fn.toolTip.defaults, options );
 
-	        // Close on ESC key
-	        $(document).on('keyup', function(event) {
-	            if(event.keyCode === 27) {
-	                $.fn.toolTip.closeTooltip();
-	            }
-	        });
+		// Close on ESC key
+		$(document).on('keyup', function(event) {
+			if(event.keyCode === 27) {
+				$.fn.toolTip.closeTooltip();
+			}
+		});
 
-			return this.each(function() {
+		return this.each(function() {
 
-				var $this      = $( this ),
-        	dark         = $this.data().dark || settings.dark,
-					width        = $this.data().width || settings.width,
-					position     = $this.data().position || settings.position,
-					close        = $this.data().close || settings.close,
-					autoclose    = $this.data().autoclose || settings.autoclose,
-					closeButton  = '<a href="#" class="close-tooltip" title="'+close+'">'+close+'</a>';
+			var $this      = $( this ),
+		dark         = $this.data().dark || settings.dark,
+				width        = $this.data().width || settings.width,
+				position     = $this.data().position || settings.position,
+				close        = $this.data().close || settings.close,
+				autoclose    = $this.data().autoclose || settings.autoclose,
+				closeButton  = '<a href="#" class="close-tooltip" title="'+close+'">'+close+'</a>';
 
 
-				if( $( window ).width() > width) {
-					$this.find('.tooltip').css({'width' : width+"px"});
-				} else {
-					$this.find('.tooltip').css({'width' : "auto"});
+			if( $( window ).width() > width) {
+				$this.find('.tooltip').css({'width' : width+"px"});
+			} else {
+				$this.find('.tooltip').css({'width' : "auto"});
+			}
+
+
+			if(position === "right") {
+				$this.find('.tooltip-layer').addClass( "tooltip-right" ); 
+			} else if (position === "center") {
+				$this.find('.tooltip-layer').addClass( "tooltip-center" );
+			} 
+
+	if(dark) {
+				$this.find('.tooltip-layer').addClass( "tooltip-layer-contrast");
+				$this.find('.tooltip').addClass( "tooltip-contrast");
+	}
+
+			$this.find('.js-tooltip-open').click( function() {
+
+				  if(autoclose) {
+					  $.fn.toolTip.closeTooltip();
 				}
 
+				  $this.find('.tooltip-layer').addClass('move-up-tooltip--mobile')
+				.removeClass('tooltip-hidden move-down-tooltip--mobile tooltip-fade-out');
 
-				if(position !== settings.position) {
+				$(document).mouseup(function (event) {
+					var container = $('.tooltip-layer');
 
-					$this.find('.tooltip-layer').addClass( "tooltip-right" );
-        }
-
-        if(dark) {
-					$this.find('.tooltip-layer').addClass( "tooltip-layer-contrast");
-					$this.find('.tooltip').addClass( "tooltip-contrast");
-        }
-
-				$this.find('.js-tooltip-open').click( function() {
-
-				  	if(autoclose) {
-				  		$.fn.toolTip.closeTooltip();
+					if (!container.is(event.target) // if the target of the click isn't the container...
+						&& container.has(event.target).length === 0) // ... nor a descendant of the container
+					{
+						$.fn.toolTip.closeTooltip();
 					}
-
-				  	$this.find('.tooltip-layer').addClass('move-up-tooltip--mobile')
-					.removeClass('tooltip-hidden move-down-tooltip--mobile tooltip-fade-out');
-
-					$(document).mouseup(function (event) {
-					    var container = $('.tooltip-layer');
-
-					    if (!container.is(event.target) // if the target of the click isn't the container...
-					        && container.has(event.target).length === 0) // ... nor a descendant of the container
-					    {
-	                		$.fn.toolTip.closeTooltip();
-					    }
-					});
-
-				});
-
-				$this.find('.tooltip').prepend(closeButton);
-
-				$this.find('.close-tooltip').click(function(event) {
-					event.preventDefault();
-					$this.find('.tooltip-layer')
-					.addClass('move-down-tooltip--mobile tooltip-fade-out')
-					.removeClass('move-up-tooltip--mobile');
 				});
 
 			});
 
-		};
+			$this.find('.tooltip').prepend(closeButton);
 
-		$.fn.toolTip.closeTooltip = function() {
-				$('.tooltip-layer').addClass('move-down-tooltip--mobile tooltip-fade-out')
+			$this.find('.close-tooltip').click(function(event) {
+				event.preventDefault();
+				$this.find('.tooltip-layer')
+				.addClass('move-down-tooltip--mobile tooltip-fade-out')
 				.removeClass('move-up-tooltip--mobile');
-		};
+			});
 
-		// Estos son los valores por defecto.
-		$.fn.toolTip.defaults = {
-			width: "480",
-			position: "left",
-			close: "Cerrar",
-			autoclose: true,
-			dark: false
-		};
+		});
+
+	};
+
+	$.fn.toolTip.closeTooltip = function() {
+			$('.tooltip-layer').addClass('move-down-tooltip--mobile tooltip-fade-out')
+			.removeClass('move-up-tooltip--mobile');
+	};
+
+	// Estos son los valores por defecto.
+	$.fn.toolTip.defaults = {
+		width: "470",
+		position: "left",
+		close: "Cerrar",
+		autoclose: true,
+		dark: false
+	};
 
 
 })( jQuery, window, document );
